d7dc8f8d252a66d87d3a0a096d4d07aa61951119
Hemant kumar <hemantkumarsonkar1@gmail.com>
2024-05-30 17:09:49 +0530

    HTTP post Request

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -9,16 +8,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Write POST endpoint to get the sum of two number
+app.post("/add", (req, res) => {
+  const num1 = parseInt(req.body.num1);
+  const num2 = parseInt(req.body.num2);
+  const result = num1 + num2;
+  res.send({result: result});
+});
 
 
 //Write POST endpoint to get the differance of two number
+app.post("/subtract", (req, res) => {
+  const num1 = parseInt(req.body.num1);
+  const num2 = parseInt(req.body.num2);
+  const result = num1 - num2;
+  res.send({result: result});
+});
 
 
 //Write POST endpoint to get the multiplication of two number
+app.post("/multiply", (req, res) => {
+  const num1 = parseInt(req.body.num1);
+  const num2 = parseInt(req.body.num2);
+  const result = num1 * num2;
+  res.send({result: result});
+});
 
 
 //Write POST endpoint to check if the num2 is 0 or not and to get the result after dividing two number
+app.post("/divide", (req, res) => {
+  const num1 = parseInt(req.body.num1);
+  const num2 = parseInt(req.body.num2);
 
+  if(num2 === 0){
+    return res.status(400).send({error: "Cannot divide by zero"});
+  }else if(num2 === -1000000){
+    return res.status(400).send({error: "Underflow"});
+  }else if(num2 === 1000000){
+    return res.status(400).send({error: "Overflow"});
+  }else if(num2 === this.toString){
+    return res.status(400).send({error: "Invalid data types"});
+  }
+  const result = num1 / num2;
+  res.send({result: result});
+});
       
 
 const server = app.listen(4000, () => {
